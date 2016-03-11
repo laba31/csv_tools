39c812dec3e3904d316d11917c1d96ff5c229a20
Ladislav Babjak <ladislav.babjak@gmail.com>
2016-03-11 20:07:33 +0100

    First commit. Already I can write help.

---
+++ csv_display.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+#Author: Ladislav Babjak
+#VERSION: 1.0
+
+use CsvMod;
+use Getopt::Std;
+getopts('hd:f:');
+
+
+sub help() {
+    print << "END_HELP";
+
+    Usage $0 [OPTION]... [filename.csv]
+Without last parameter filename script read standard input
+
+        -h      print this help message
+        -d      set other then default delimiter ';' for .csv file
+        -f      which of the columns will be displayed
+                1-3 or 1:3 are same for range of values
+                4,2,8 columns for selection
+                id,name,age columns for selection by name
+
+Some examples:
+
+$0 -d ',' -f 1-5
+$0 -f 2,3,5
+$0 -d '\t' -f 1:3
+$0 -f id,name,age
+
+END_HELP
+}
+
+
+
+if($opt_h) {
+        &help();
+        exit 0;
+}
+
