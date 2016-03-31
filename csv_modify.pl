e15c60857dbd2e4ff45829f8e005d559a303dbe6
Ladislav Babjak <ladislav.babjak@gmail.com>
2016-03-31 20:20:27 +0200

    Functional working version under testing.

--- csv_modify.pl
+++ csv_modify.pl
@@ -6,14 +6,23 @@ use Getopt::Std;
 getopts('hbr:f:');
 
 
+# my favorit delimiter
+my $delimiter = ';';
+# head of csv file
+my @head = undef;
+my $line = undef;
+my @columns = undef;
+my $work_file = undef;
+
+
 sub help() {
     print << "END_OF_HELP";
 
-Usage $0 -r 'perl_regexp' -f which_columns [filename.csv]
-Without last parameter filename.csv script read standard input.
+Usage $0 -r 'perl_regexp' -f which_columns [-b] filename.csv
 
     -h  print this help message
     -b  copy original file with sufix .bkp
+    -d  set other then default delimiter ';' for .csv file
 
 Some examples:
 
@@ -23,7 +32,7 @@ $0 -r '/YES/NO/' -f 2:6     string NO replacing string YES from column 2 to colu
 $0 -r '/YES/NO/' -f 2-6     same as previous example
 
 $0 -r '/.*//' -f 1          delete value in whole column 1
-$0 -r '/^.*$/YES/' -f 1     everything setting on YES, including empty value
+$0 -r '/^.*\$/YES/' -f 1     everything setting on YES, including empty value
 
 END_OF_HELP
 }
@@ -38,4 +47,62 @@ if((! $::opt_f) and (! $::opt_r)) {
     &help();
     exit 0;
 }
+
+if($::opt_d) {
+        $delimiter=$::opt_d;
+}
+
+
+if(@ARGV == 1) {
+    open(FD, $ARGV[0]) or die "I can not open file $ARGV[0]\n";
+    $work_file = $ARGV[0] . ".tmp";
+    open(WD, '>', $work_file) or die "I can not create file $work_file\n";
+}
+else {
+    print "Missing parameter filename.csv\n";
+    exit 1;
+}
+
+# first line = head
+$line=<FD>;
+# write head to new file without changes
+print WD $line;
+@head = &parse_line($delimiter, $line);
+@columns = &which_columns($::opt_f, \@head);
+
+if((&check_range(\@columns, \@head)) == -1) {
+    print "Range of choice fields is wrong.\n";
+    exit 1;
+}
+
+
+my $regexp = "s" . $::opt_r . "g";
+
+while($line=<FD>) {
+    my @full_list = &parse_line($delimiter, $line);
+    foreach my $item (@columns) {
+        my $run_string = '$full_list[$item] =~ ' . $regexp . ';';
+        eval $run_string;
+        if($@) {
+            print "Bad regular expression!\n";
+            print $@;
+            exit 1;
+        }
+    }
+    my $new_line = join($delimiter, @full_list) . "\n";
+    print WD $new_line;
+}
+
+
+close(FD);
+close(WD);
+
+# backup original file
+if($::opt_b) {
+    my $bkp_file = $ARGV[0] . ".bkp";
+    rename $ARGV[0], $bkp_file or die "I can not rename file $ARGV[0] to $bkp_file\n";
+}
+
+# rename temporary file to original
+rename $work_file, $ARGV[0] or die "I can not rename file $work_file to $ARGV[0]\n";
 
