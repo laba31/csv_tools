87fd5fce3cbabc7a401a591c3b9d116306a9d1dc
Ladislav Babjak <ladislav.babjak@gmail.com>
2016-04-01 20:28:39 +0200

    Only comments.

--- csv_modify.pl
+++ csv_modify.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/env perl
+#Author: Ladislav Babjak
+#VERSION: 1.0
 
 use strict;
 use CsvMod;
@@ -10,8 +12,11 @@ getopts('hbr:f:');
 my $delimiter = ';';
 # head of csv file
 my @head = undef;
+# for reading line from file
 my $line = undef;
+# which columns selected
 my @columns = undef;
+# temporary file for changes
 my $work_file = undef;
 
 
@@ -43,16 +48,18 @@ if($::opt_h) {
     exit 0;
 }
 
+# required parameters
 if((! $::opt_f) and (! $::opt_r)) {
     &help();
     exit 0;
 }
 
+# set other then default(;) delimiter
 if($::opt_d) {
         $delimiter=$::opt_d;
 }
 
-
+# .csv filename is required too
 if(@ARGV == 1) {
     open(FD, $ARGV[0]) or die "I can not open file $ARGV[0]\n";
     $work_file = $ARGV[0] . ".tmp";
@@ -63,46 +70,60 @@ else {
     exit 1;
 }
 
-# first line = head
+# first line = header
 $line=<FD>;
-# write head to new file without changes
+
+# write header to new file without changes
 print WD $line;
+
+# header in array
 @head = &parse_line($delimiter, $line);
+
+# which columns selected
 @columns = &which_columns($::opt_f, \@head);
 
+# check range of columns
 if((&check_range(\@columns, \@head)) == -1) {
     print "Range of choice fields is wrong.\n";
     exit 1;
 }
 
-
+# assembling of regular expression
 my $regexp = "s" . $::opt_r . "g";
 
+# main loop - read every line of file
 while($line=<FD>) {
+    # whole row in array
     my @full_list = &parse_line($delimiter, $line);
+    # loop for selected columns
     foreach my $item (@columns) {
+        # assembling of cmd for eval
         my $run_string = '$full_list[$item] =~ ' . $regexp . ';';
         eval $run_string;
+        # handling of error
         if($@) {
             print "Bad regular expression!\n";
+            # print perl error
             print $@;
             exit 1;
         }
     }
+    # assembling changed line for temporary file
     my $new_line = join($delimiter, @full_list) . "\n";
     print WD $new_line;
 }
 
-
+# Let's be decent...
 close(FD);
 close(WD);
 
 # backup original file
 if($::opt_b) {
+    # name for backup file
     my $bkp_file = $ARGV[0] . ".bkp";
+    # rename sorce file to .bkp file
     rename $ARGV[0], $bkp_file or die "I can not rename file $ARGV[0] to $bkp_file\n";
 }
 
 # rename temporary file to original
 rename $work_file, $ARGV[0] or die "I can not rename file $work_file to $ARGV[0]\n";
-
